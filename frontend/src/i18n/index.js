dafcf67e4a4afa9ccf2128b4849901ddf388f11c
Vantablack <42588977+Vantablacker@users.noreply.github.com>
2024-10-11 13:51:03 +0200

    Update index.js

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -2,10 +2,12 @@ import enUS from './en-US'
 import zhCN from './zh-CN'
 import deDE from './de-DE'
 import frFR from './fr-FR'
+import esES from './es-ES'
 
 export default {
   'en-US': enUS,
   'zh-CN': zhCN,
   'de-DE': deDE,
-  'fr-FR': frFR
+  'fr-FR': frFR,
+  'es-ES': esES
 } 
